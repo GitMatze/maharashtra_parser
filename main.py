ad84d57b36fefa91298da7300a876723f5b00c3c
GitMatze <45139485+GitMatze@users.noreply.github.com>
2020-02-27 09:51:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import arrow
 import time
+import datetime
+import logging
 
 url = 'https://mahasldc.in/wp-content/reports/sldc/mvrreport3.jpg'
 
@@ -18,14 +20,14 @@ locations = {
 	},
 	'MS SOLAR' : {
 			'label' : (595,963,705,984),
-			'value' : (797,959,848,983)
+			'value' : (785,959,848,983)
 	},
 	'THERMAL' : {
 			'label' : (407,982,502,1004),
-			'value' : (516,987,585,1015)
+			'value' : (516,987,584,1015)
 	},
 	'GAS' : {
-			'label' : (403,1033,493,1054),
+			'label' : (403,1033,493,1056),
 			'value' : (515,1038,582,1068)
 	},
 	'HYDRO' : {
@@ -39,10 +41,6 @@ locations = {
 	'TPC THM.' : {
 			'label' : (924,578,1030,604),
 			'value' : (1088,581,1173,605)
-	},
-	'GHATGR PUMP' : {
-			'label' : (594,671,735,697),
-			'value' : (789,675,828,700)
 	},
 	'COGEN' : {
 			'label' : (594,989,670,1011),
@@ -59,9 +57,122 @@ locations = {
 	'CS GEN. TTL.' : {
 			'label' : (1341,998,1492,1029),
 			'value' : (1549,1001,1616,1023)
+	},
+	'KK’ PARA' : {
+			'label' : (1346,708,1457,730),
+			'value' : (1560,711,1626,732)
+	},
+	'TARPR PH-I' : {
+			'label' : (1341,730,1484,757),
+			'value' : (1556,733,1626,755)
+	},
+	'TARPR PH-II' : {
+			'label' : (1341,750,1490,782),
+			'value' : (1556,757,1626,781)
+	},
+	'SSP' : {
+			'label' : (1341,800,1401,823),
+			'value' : (1557,802,1626,825)
+	},
+	'RGPPL' : {
+			'label' : (1343,822,1421,849),
+			'value' : (1560,820,1620,850)
+	},
+	'GANDHAR' : {
+			'label' : (1341,660,1446,689),
+			'value' : (1562,656,1623,685)
+	},
+	'CS EXCH' : {
+			'label' : (920,303,1021,338),
+			'value' : (1090,309,1172,334)
+	},
+	#STATE DEMAND (including Mumbai!)
+	'DEMAND' : {
+			'label' : (932,1003,1021,1030),
+			'value' : (1080,996,1167,1018)
+	},
+	#RE TTL
+	'TTL' : {
+			'label' : (597,1035,663,1056),
+			'value' : (783,1032,845,1056)
+	},
+	'TTL (IPP/CPP+RE)' : {
+			'label' : (594,1072,765,1098),
+			'value' : (786,1068,847,1093)
+	},
+	'SOLAR TTL' : {
+			'label' : (592,577,715,605),
+			'value' : (772,583,814,602)
+	},
+	'PIONEER' : {
+			'label' : (592,910,694,929),
+			'value' : (794,910,844,927)
 	}
 }
 
+generation_map = {
+	'biomass' : {
+		'add': ['COGEN'],
+		'subtract': []
+	},
+    'coal' : {
+		'add': ['THERMAL',
+				'TTL (IPP/CPP+RE)',
+				'TPC THM.',
+				'CS GEN. TTL.',
+				'AEML GEN.',
+				'SOLAR TTL'],
+		'subtract': ['TTL',
+					 'PIONEER',
+					 'SSP',
+					 'RGPPL',
+					 'TARPR PH-I',
+					 'TARPR PH-II',
+					 'KK’ PARA',
+					 'GANDHAR']
+	},
+	'gas': {
+		'add': ['GAS',
+				'PIONEER',
+				'GANDHAR',
+				'RGPPL'],
+		'subtract': []
+	},
+	'hydro' : {
+		'add': ['HYDRO',
+				'TPC HYD.',
+				'SSP'],
+		'subtract': []
+	},
+	'nuclear' : {
+		'add': ['TARPR PH-I',
+				'TARPR PH-II',
+				'KK’ PARA'],
+		'subtract': []
+	},
+	'solar' : {
+		'add': ['MS SOLAR'],
+		'subtract': []
+	},
+    'wind' : {
+		'add': ['MS WIND'],
+		'subtract': []
+	},
+	'unknown' : {
+		'add': ['OTHR+SMHYD'],
+		'subtract': []
+	}
+
+}
+
+#list of values that belong to Central State production
+CS = ['SSP',
+	 'RGPPL',
+	 'TARPR PH-I',
+	 'TARPR PH-II',
+	 'KK’ PARA',
+	 'GANDHAR',
+	 'CS GEN. TTL.']
 
 #converts image into a black and white image
 def RGBtoBW(pil_image):
@@ -79,28 +190,132 @@ def recognize(location, source, lang):
 
 	return text, img
 
+# ignores highly indistinguishable characters
+def areEqual(str1,str2):
+	chars = {
+		'{':'(',
+		'}': ')',
+		'T': 'L',
+		'I': 'L',
+		' ': ''
+	}
+	for key, value in chars.items():
+		str1 = str1.replace(key,value)
+		str2 = str2.replace(key,value)
+	return str1.lower() == str2.lower()
+
+
+def fetch_production(zone_key='GE', session=None, target_datetime: datetime.datetime = None,
+					 logger: logging.Logger = None):
+
+
+	if target_datetime is not None:
+		raise NotImplementedError('This parser is not yet able to parse past dates')
+
+
+	time = arrow.now('Asia/Kolkata').floor('minute').datetime
+	data = {
+		'zoneKey': '',
+		'datetime': time,
+		'production': {
+			'biomass': 0.0,
+			'coal': 0.0,
+			'gas': 0.0,
+			'hydro': 0.0,
+			'nuclear': 0.0,
+			'solar': 0.0,
+			'wind': 0.0,
+			'unknown': 0.0
+      	},
+		'storage': {},
+		'source': 'https://mahasldc.in',
+	}
+
+	image = imread(url)
+	image = Image.fromarray(image)  # create PIL image
+
+	labels = {}
+	values = {}
+
+	#read in label-value pairs from the image as specified in locations dict
+	for type, locs in locations.items():
+		label,_ = recognize(locs['label'], image, 'eng')
+		value, _ = recognize(locs['value'], image, 'digits_comma')
+		labels[type] = label
+		values[type] = max( [float(value),0] )
+
+		assert (areEqual(label,type)), \
+			'Wrongly regognized label `{}` as `{}`'.format(type, label)
+
+	# fraction of central state production that is exchanged with Maharashtra
+	share = values['CS EXCH'] / values['CS GEN. TTL.']
+
+	for type, plants in generation_map.items():
+		for plant in plants['add']:
+			fac = share if plant in CS else 1 # add only a fraction of central state plant production
+			data['production'][type] += fac * values[plant]
+		for plant in plants['subtract']:
+			fac = share if plant in CS else 1
+			data['production'][type] -= fac * values[plant]
+
+	#Sum over all production types is expected to equal the total demand
+	demand_diff = sum( data['production'].values() ) - values['DEMAND']
+	assert (abs( demand_diff) < 5), \
+		'Production types do not add up to total demand. Difference: {}'.format(demand_diff)
+
+	return data
+
+
 
 if __name__ == '__main__':
 
-	file = open('logfile_ext.txt', 'a')
-	
+	#FOR DEBUGGING
+	#logs results in a logfile, saves an image of all ocr tasks and saves the dashboard image in case of failure
+
+	#data =fetch_production(zone_key=None, session=None, target_datetime = None, logger = None)
+	#import os,sys
+	#dirs = os.listdir('dashboards')
+
+	file = open('log.txt', 'a')
+
+	# write column names
+	for key in locations.keys():
+		file.write(key.replace(' ', '_') + ' ')
+	for key in generation_map.keys():
+		file.write(key + ' ')
+
+	file.write('localtime' + ' ')
+	file.write('rec_time' + '\n')
+
+
 	#read image and save to a logfile
 	while (True):
+	#for dir in dirs:
+
+		results = {
+			'biomass': 0.0,
+			'coal': 0.0,
+			'gas': 0.0,
+			'hydro': 0.0,
+			'nuclear': 0.0,
+			'solar': 0.0,
+			'wind': 0.0,
+			'unknown': 0.0}
 
 		image = imread(url)
 		image = Image.fromarray(image)  # create PIL image
-		#image = Image.open('error_2020-02-22T19 31.png')
+		#image = Image.open('dashboards/'+'error_2020-02-26T14 43.png')
 
 		line = ''
-		labels = []
-		values=[]
+		labels = {}
+		values = {}
 
 		localtime = arrow.utcnow().shift(hours=5, minutes=30)
 		localtime = localtime.format('YYYY-MM-DDTHH:mm')
-		save_time = localtime.replace(':', ' ')
+		filename_time = localtime.replace(':', ' ')
 
 		plt_num = 1
-		fig = plt.figure(figsize=(3,13))
+		fig = plt.figure(figsize=(3,23))
 		plt.subplots_adjust(top=0.8, wspace=0.2, hspace=0.3)
 		rows = len(locations)
 		cols = 2
@@ -109,8 +324,8 @@ if __name__ == '__main__':
 		for type, locs in locations.items():
 			label, l_img = recognize(locs['label'], image, 'eng')
 			value, v_img = recognize(locs['value'], image, 'digits_comma')
-			labels.append(label)
-			values.append(value)
+			labels[type] = label
+			values[type] = float(value)
 
 			axes = fig.add_subplot(rows, cols, plt_num)
 			axes.get_xaxis().set_visible(False)
@@ -126,20 +341,35 @@ if __name__ == '__main__':
 			plt.title(value)
 			plt_num = plt_num + 1
 
+		share = values['CS EXCH'] / values['CS GEN. TTL.']
+		for type, plants in generation_map.items():
+			for plant in plants['add']:
+				fac = share if plant in CS else 1  # add only a fraction of central state plant production
+				results[type] += fac * values[plant]
+			for plant in plants['subtract']:
+				fac = share if plant in CS else 1
+				results[type] -= fac * values[plant]
+
 		#create line for log.txt
 		#compare recognized label with name in locations dict to detect errors
-		for i, key in enumerate(locations.keys()):
-			if labels[i] == key:
-				line = line+values[i]+ ' '
-			else:
-				line = line + 'NaN '
-				image.save('error_'+save_time+'.png')
-				print('Error: ' + labels[i] + ', ' +key)
+		for key in locations.keys():
+			line = line+str(values[key])+ ' '
+			if not areEqual( labels[key], key ):
+				image.save('error_'+filename_time+'.png')
+				print('Error: ' + labels[key] + ', ' +key)
+
+		demand_diff = sum(results.values()) - values['DEMAND']
+		if (abs(demand_diff) > 5):
+			image.save('error_' + filename_time + '.png')
+			print('Error: Demand Difference = {}'.format(demand_diff))
+
+
+
+		for value in results.values():
+			line = line + str(value) + ' '
 
 		#read daytime from image
 		rec_time, img = recognize( (355,110,524,150), image, 'eng')
-		#plt.imshow(img)
-		#plt.title(rec_time)
 		rec_time = rec_time.replace(' ', 'T')
 
 
@@ -147,9 +377,23 @@ if __name__ == '__main__':
 		line = line + rec_time
 		file.write(line+'\n')
 		print(line)
-		plt.savefig('figures/' + save_time + '.png')
+		#print(results)
+		plt.savefig('figures/' + filename_time + '.png')
 		#plt.show()
 		time.sleep(60*10)
 		plt.close()
 
 	file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
